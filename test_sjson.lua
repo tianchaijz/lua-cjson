257518120d8a83723d6f72585bb5c3ba96f9a904
Jinzheng Zhang <tianchaijz@gmail.com>
2019-04-21 09:59:30 +0000

    Reduce big number limit.

--- test_sjson.lua
+++ test_sjson.lua
@@ -48,3 +48,30 @@ local t = {
 }
 
 print(sjson.encode(t))
+
+local decoder = sjson.new()
+decoder.decode_big_numbers_as_strings(true)
+
+print(sjson.decode("9"))
+print(sjson.decode("99"))
+print(sjson.decode("999"))
+print(sjson.decode("9999"))
+print(sjson.decode("99999"))
+print(sjson.decode("999999"))
+print(sjson.decode("9999999"))
+print(sjson.decode("99999999"))
+print(sjson.decode("999999999"))
+print(sjson.decode("9999999999"))
+print(sjson.decode("99999999999"))
+print(sjson.decode("999999999999"))
+print(sjson.decode("9999999999999"))
+print(sjson.decode("99999999999999"))
+print("----")
+print(sjson.decode("100000000000000"))
+print(sjson.decode("999999999999999"))
+
+assert(decoder.decode("10000000000000") == 10000000000000)
+assert(decoder.decode("99999999999999") == 99999999999999)
+assert(decoder.decode("100000000000000") == "100000000000000")
+assert(decoder.decode("999999999999999") == "999999999999999")
+assert(decoder.decode("9007199254740992") == "9007199254740992" )
